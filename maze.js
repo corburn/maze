cb932314c9621e8dca044e7555e58bca592999cb
Jason Travis <infomaniac7@gmail.copm>
2013-04-07 10:21:06 -0700

    update: preprocessed solution path
    Use a preprocessed solution path for hints. If the moves are in the
    correct direction, remove them from the solution path. Otherwise, add
    the opposite direction to the solution path.

--- maze.js
+++ maze.js
@@ -16,22 +16,14 @@
  * TODO
  */
 (function() {
-
-    // TODO
-    // Check for web worker support
-    if(typeof(Worker)==="undefined") {
-        alert("Your browser does not support web workers");
-        return;
-    }
-
+    // preprocessed solution path
+    var solution = [2,2,2,2,3,0,0,0,3,2,3,3,3,2,3,3,0,1,0,0,3,2,3,3,3,0,3,2,3,3,3,2,1,1,1,1,2,2,2,1,2,1,2,1,2,3,2,3,0,0,3,0,3,0,3,0,1,0,3,3,3,3,3,2,1,1,1,2,2,2,2,3,3,2,1,2,3,2,1,1,0,0,1,0,0,1,2,2,1,2,3,3,2,1,2,3,2,1,1,2,3,3,3,0,3,3,2,2,1,2,3,3,2,2,2,1,0,1,2,1,0,0,0,1,1,1,0,1,0,0,1,0,3,0,1,1,1,2,1,2,1,1,1,0,3,0,3,0,0,1,1,0,1,0,3,0,1,1,2,2,2,3,2,2,1,2,2,3,2,3,2,2,3,3,2,2,2,1,1,2,2,3,3,3,0,0,3,2,2,3,0,0,0,1,0,0,3,2,3,0,3,2,2,2,1,2,2,3,0,3,2,3,2,3,3,2,2,2,3,2,2,2,3,3,0,1,0,3,3,0,0,1,1,0,3,0,0,0,3,2,3,3,2,2,1,2,2,2,2,3,3,2,2,2,3,0,0,0,3,3,3,2,1,1,2,2,3,3,2,1,1,1,2,2,2,3,0,0,3,2,3,0,3,0,0,3,2,3,0,3,0,0,3,2,3,0,0,3,2,2,2,1,1,2,1,2,3,2,1,2,3,3,0,3,3,0,0,0,3,3,2,3,0,0,1,1,0,1,0,3,3,2,3,0,3,0,0,0,1,1,1,2,2,1,1,1,1,2,1,0,1,1,2,2,2,1,0,0,0,1,1,1,0,3,0,1,0,0,1,2,1,0,0,0,0,3,2,2,3,3,3,3,0,1,0,3,3,3,2,3,0,0,1,1,1,1,1,0,0,3,2,3,0,0,1,1,1,0,1,1,1,0,1,1,1,0,0,0,0,3,2,2,3,0,3,0,0,1,0,1,1,0,3,0,0,0,3,3,0,0,3,2,3,0,0,3,3,3,3,3,2,2,3,0,3,0,3,2,3,0,3,3,3,2,2,3,3,2,3,3,2,3,3,0,1,0,1,1,0,0,3,2,3,0,3,3,2,3,0,3,3,3,3,2,2,3,2,1,2,2,3,0,3,2,2,1,1,2,1,0,0,1,0,3,0,0,1,2,1,2,2,1,0,1,2,1,1,2,2,3,0,3,3,2,1,2,3,2,1,2,1,0,1,0,1,1,0,1,0,1,2,2,1,0,1,2,2,2,2,3,0,0,3,2,3,2,1,2,1,1,1,0,0,0,0,0,0,0,0,1,0,0,1,2,1,1,1,1,0,0,1,2,1,0,1,2,2,1,2,3,2,1,1,2,2,3,2,3,2,3,0,0,0,3,2,2,3,0,3,0,3,0,3,2,3,2,1,1,2,2,2,2,2,2,1,2,3,3,3,0,3,2,3,3,2,3,0,3,3,2,2,2,1,0,1,2,1,1,2,2,2,2,3,2,3,2,3,3,0,1,0,0,3,3,3,0,1,1,0,3,0,0,3,2,3,2,3,3,3,3,2,3,2,1,2,2,3,0,3,0,0,3,2,2,2,1,2,2,2,1,2,3,2,3,0,0,0,0,3,3,0,0,1,0,0,3,3,3,2,2,3,2,1,2,3,2,3,0,0,3,0,0,0,3,2,3,2,2,1,2,3,2,3,0,0,0,3,0,0,1,1,0,1,1,1,2,1,0,1,1,1,0,1,2,1,1,0,3,0,3,0,0,0,0,3,0,3,2,3,2,3,0,3,3,0,0,3,3,2,1,2,2,2,1,0,1,2,2,2,2,3,3,3,0,1,0,3,3,2,3,2,1,2,3,3,2,3,2,3,0,0,1,0,1,0,3,0,1,0,3,3,0,3,2,3,0,3,2,2,3,2,1,1,0,1,1,2,3,2,3,3,3,3,0,3,0,3,0,1,1,0,0,0,3,0,0,3,2,2,3,2,3,3,3,0,0,0,0,3,0,1,1,1,2,1,0,1,1,2,1,1,1,0,0,3,3,0,3,0,3,3,0,1,1,1,2,1,2,1,0,1,2,2,2,2,1,2,1,0,1,0,3,3,0,0,0,0,0,0,0,3,3,2,2,1,2,3,3,0,3,0,3,2,3,0,3,2,3,3,2,2,2,2,3,3,3,0,0,3,3,0,0,1,2,1,0,0,1,1,0,3,3,3,3,2,3,3,2,2,1,2,3,2,3,2,2,2,3,2,2,3,2,3,0,0,3,2,2,3,3,2,1,1,1,1,2,3,2,3,2,3,0,3,0,3,3,0,1,0,3,3,3,3,2,2,2,1,1,1,1,2,2,3,3,0,3,2,3,2,3,0,0,0,0,3,2,3,2,1,2,2,2,1,1,2,2,1,0,1,2,1,0,1,0,0,1,0,1,1,1,1,2,2,3,0,3,2,2,2,2,3,3,0,3,2,2,1,1,1,1,2,1,0,1,0,3,0,1,1,2,1,0,0,1,0,1,0,0,1,0,0,0,3,0,3,2,3,2,3,0,3,0,1,1,0,3,0,1,1,0,0,1,2,1,1,2,2,1,2,2,2,2,2,2,1,2,3,2,2,1,2,2,3,2,1,2,1,1,0,3,0,0,1,2,1,2,2,2,1,1,1,0,0,0,1,2,2,1,2,3,2,3,3,2,1,2,2,2,1,1,0,0,3,0,1,1,2,2,1,1,0,1,0,1,0,3,0,0,3,2,2,3,3,0,0,0,3,0,1,0,3,3,0,1,1,1,2,1,1,1,2,3,2,1,2,1,2,1,2,2,3,2,3,2,1,2,3,2,1,2,3,2,3,2,3,3,3,3,0,3,3,0,3,2,2,1,2,3,3,0,0,0,3,2,2,3,2,1,2,1,2,2,3,3,2,1,1,1,0,1,2,2,3,3,2,3,2,3,3,0,0,3,0,1,0,0,0,0,3,0,1,0,1,0,3,3,3,3,0,0,1,2,1,1,1,1,0,1,0,1,0,3,0,3,2,3,3,3,0,1,0,3,0,3,3,3,2,1,2,3,2,1,2,3,2,2,2,2,2,2,2,1,1,1,2,2,3,3,0,3,2,2,2,1,1,2,2,3,0,3,3,0,0,3,0,1,0,3,0,3,3,3,3,2,3,0,0,1,0,3,3,2,2,3,3,2,1,1,2,2,2,2,3,3,3,2,1,1,2,1,1,0,0,1,1,0,0,0,1,1,2,3,2,1,1,2,3,2,1,1,1,2,3,3,3,2,2,3,0,0,0,3,2,3,2,3,3,2,2,2,3,0,0,0,3,0,3,3,2,3,0,0,1,0,3,0,1,1,0,0,0,0,0,1,0,1,0,0,3,0,1,1,1,2,3,2,1,1,1,0,1,0,1,2,1,0,0,1,0,3,3,0,3,0,1,1,2,1,0,0,3,0,0,0,1,2,1,0,1,0,3,3,3,3,3,2,3,2,2,3,3,2,3,0,0,3,2,2,2,3,2,2,3,2,3,3,0,0,0,1,0,3,3,2,2,3,2,2,3,2,3,2,2,3,3,3,3,2,1,1,1,1,2,2,1,2,3,2,1,1,0,1,1,0,0,3,3,0,1,1,0,0,0,1,1,2,2,2,3,2,2,2,2,3,3,2,2,2,2,2,2,3,2,2,2,3,2,2,1,1,0,1,0,1,0,3,0,1,1,2,2,1,2,3,2,3,2,2,2,3,3,3,3,2,1,2,1,0,1,1,1,2,1,0,1,2,2,1,2,2,3,2,3,0,3,2,3,2,1,1,1,1,1,1,0,0,0,3,0,0,1,1,2,1,0,1,2,2,1,0,0,1,2,2,2,1,2,2,3,2,1,1,2,1,2,3,2,3,2,2,1,2,1,1,2,1,2,1,2,2,3,3,3,3,0,3,2,2,1,1,2,2,2,3,3,0,1,0,3,3,2,2,3,2,3,3,0,0,0,3,2,2,3,0,3,3,2,1,2,2,2,3,2,2,2,2,2,3,2,3,3,2,3,3,0,0,3,3,2,2,2,1,2,2,3,0,3,3,2,2,3,3,3,2,3,0,3,2,2,1,1,2,3,3,3,0,0,0,3,2,3,2,2,3,2,1,1,2,3,3,2,2,1,1,1,1,0,1,2,2,2,3,0,3,2,3,3,3,2,2];
     // maze canvas element
     var canvas = document.getElementById('maze');
     // canvas context
     var ctx = canvas.getContext('2d');
     // maze image
     var maze = new Image();
-    // TODO
-    var worker = new Worker("worker.js");
 
     // Direction enum
     var Direction = {"left":0,"up":1,"right":2,"down":3};
@@ -40,8 +32,8 @@
     // exit coordinates
     var Exit = {"x":797,"y":799};
 
-	// size of marker
-	var m = 4;
+    // size of marker
+    var m = 4;
     // marker x position
     var x = Start['x'];
     // marker y postion
@@ -50,8 +42,6 @@
     var dx = m + m;
     // how much y is changed when an arrow key is pressed
     var dy = m + m;
-	// path taken
-	var path = [];
     // marker color
     var marker = "Purple";
     // trail color
@@ -77,10 +67,11 @@
 
     /**
      * compare returns true if the pixel is the same color as the rgba array.
-	 *
-	 * @param pixel a pixel object
-	 * @param rgba an array representing a color using rgba values
+     *
+     * @param pixel a pixel object
+     * @param rgba an array representing a color using rgba values
      * rgba = [red,green,blue,alpha]
+     * @return true if pixel is the same color
      */
     var compare = function(pixel,rgba) {
         return pixel[0] === rgba[0] && pixel[1] === rgba[1] && pixel[2] === rgba[2] && pixel[3] === rgba[3];
@@ -89,42 +80,47 @@
     /**
      * doKeyDown moves the marker when an arrow key is pressed
      * or requests a hint when 'h' is pressed.
-	 *
-	 * @param evt the event passed by the event listener
+     *
+     * @param evt the event passed by the event listener
      */
     var doKeyDown = function(evt) {
-		var direction = -1;
+        var direction = null;
 
-		// Hide hint on next keypress
+        // Hide hint on next keypress
         hint(false);
         switch (evt.keyCode) {
         case 38:	// Up arrow was pressed
-			direction = Direction["up"];
+            direction = Direction["up"];
             break;
         case 40:	// Down arrow was pressed
-			direction = Direction["down"];
+            direction = Direction["down"];
             break;
         case 37:	// Left arrow was pressed
-			direction = Direction["left"];
+            direction = Direction["left"];
             break;
         case 39:	// Right arrow was pressed
-			direction = Direction["right"];
+            direction = Direction["right"];
             break;
         case 72:	// 'h' was pressed
             hint(true);
             break;
         }
-		// If an arrow key was pressed, move in that direction and record it in the path array
-		if(direction != -1) {
-			move(direction);
-			path.push(direction);
-		}
+        // If an arrow key was pressed and the marker moved
+        if(direction != null && move(direction)) {
+            if(direction == solution[0]) {
+				// If it was in the correct direction remove it from the solution path
+                solution.shift();
+            } else {
+				// else add the opposite direction to the solution path
+                solution.unshift((direction+2)%4);
+            }
+        }
     }
 
     /**
      * draw the marker at the current position.
-	 *
-	 * @param color the marker color
+     *
+     * @param color the marker color
      */
     var draw = function(color) {
         ctx.fillStyle = color;
@@ -139,9 +135,10 @@
      *
      * @param direction the direction to check
      * @param gap the number of pixels from the marker
+     * @return pixel at the relative position
      */
     var getPixel = function(direction, gap) {
-		var pixel;
+        var pixel;
         switch(direction) {
         case Direction["up"]:
             pixel = ctx.getImageData(x, y - gap, 1, 1).data;
@@ -165,44 +162,42 @@
 
     /**
      * hint toggles hint.
-	 * TODO improve performance by only calculating as far as necessary.
-	 * Perhaps a breadth first algorithm? Current runtime is approximately
-	 * 1 second on the dev machine.
      *
      * @param show display hint when true
      */
     var hint = function(show) {
         if(show) {
-			var direction;
-        	document.getElementById("hint").value = "Calculating hint...";
-			// Calculate path from current position to exit
-            solution = tremaux();
-			// Restore canvas state
-			x = Start['x'];
-			y = Start['y'];
-        	ctx.drawImage(maze,0,0);
-			draw(marker);
-			path.forEach(move);
-			// Convert first solution path direction to a string
-			switch(solution[0]) {
-				case Direction["up"]:
-					direction = "up";
-					break;
-				case Direction["down"]:
-					direction = "down";
-					break;
-				case Direction["left"]:
-					direction = "left";
-					break;
-				case Direction["right"]:
-					direction = "right";
-					break;
-			}
-			// Display hint
-        	document.getElementById("hint").value = direction;
+            var direction = "Error: Invalid direction";
+            document.getElementById("hint").value = "Calculating hint...";
+            // Calculate path from current position to exit
+            //solution = tremaux();
+            //console.log(solution.toString());
+            // Restore canvas state
+            //x = Start['x'];
+            //y = Start['y'];
+            //ctx.drawImage(maze,0,0);
+            //draw(marker);
+            //path.forEach(move);
+            // Convert first solution path direction to a string
+            switch(solution[0]) {
+            case Direction["up"]:
+                direction = "up";
+                break;
+            case Direction["down"]:
+                direction = "down";
+                break;
+            case Direction["left"]:
+                direction = "left";
+                break;
+            case Direction["right"]:
+                direction = "right";
+                break;
+            }
+            // Display hint
+            document.getElementById("hint").value = direction;
         } else {
-			// Hide hint
-        	document.getElementById("hint").value = "";
+            // Hide hint
+            document.getElementById("hint").value = "";
         }
     }
 
@@ -210,6 +205,7 @@
      * isWall returns true if there is a maze wall in the given direction.
      *
      * @param direction the direction to check for a wall
+     * @return true if the pixel is black
      */
     var isWall = function(direction) {
         var gap = 2; // gap between marker and wall
@@ -223,15 +219,16 @@
      * move the marker in the given direction if there is no wall in the way.
      *
      * @param direction the direction to move
+	 * @return true if move successful
      */
     var move = function(direction) {
-		// Cannot move, there is a wall
-		if(isWall(direction)) return;
+        // Cannot move, there is a wall
+        if(isWall(direction)) return false;
 
         // Leave a trail of previous markers
         draw(trail);
 
-		// Move the marker
+        // Move the marker
         switch(direction) {
         case Direction["up"]:
             y -= dy;
@@ -247,11 +244,12 @@
             break;
         default:
             console.log("move: " + direction + " is an invalid direction");
-            return;
+            return false;
         }
 
         // Draw the marker a different color so it can be distinguished from the trail
         draw(marker);
+		return true;
     }
 
     /**
@@ -261,13 +259,15 @@
      * If there is no unmarked path and the current path has only been marked once,
      * turn around. Otherwise, choose a path with the fewest marks. Paths marked exactly
      * once lead back to the start.
+     *
+     * @return solution path
      */
     var tremaux = function() {
-		// markedOnce returns true if the pixel is marked with the first traversal color
+        // markedOnce returns true if the pixel is marked with the first traversal color
         var markedOnce = function(pixel) {
             return compare(pixel,[0,128,0,255]);
         }
-		// markedTwice returns true if the pixel is marked with the second traversal color
+        // markedTwice returns true if the pixel is marked with the second traversal color
         var markedTwice = function(pixel) {
             return compare(pixel,[255,0,0,255]);
         }
@@ -275,46 +275,48 @@
         var onceTrail = "rgba(0,128,0,255)";;
         // second traversal color
         var twiceTrail = "rgba(255,0,0,255)";
-		var solution = [];
-		// an arbitrary starting direction
+        // an arbitrary starting direction
         var direction = Direction["up"];
+        // solution path
+        var solution = [];
+        // the pixel we're checking to see if the path has been marked
         var pixel;
 
-		// Redraw the maze so nothing interferes with the search
+        // Redraw the maze so nothing interferes with the search
         ctx.drawImage(maze,0,0);
 
-		// Search until we find the exit
+        // Search until we find the exit
         while(x !== Exit['x'] || y !== Exit['y']) {
-			// Leave a trail of paths explored.
+            // Leave a trail of paths explored.
             trail = onceTrail;
-			// Check all directions twice. The first check finds unexplored paths.
-			// The second check finds the way back to the solution path, marking
-			// the area behind us as a dead end.
+            // Check all directions twice. The first check finds unexplored paths.
+            // The second check finds the way back to the solution path, marking
+            // the area behind us as a dead end.
             for(var i = 0; i < 8; i++) {
                 direction = (direction + i) % 4;
                 if(isWall(direction)) continue;
                 pixel = getPixel(direction, m + 1);
-				// Find unexplored paths
+                // Find unexplored paths
                 if(!markedOnce(pixel) && !markedTwice(pixel)) break;
-				// On the second check, find a path that hasn't been marked as a dead end
+                // On the second check, find a path that hasn't been marked as a dead end
                 if(i > 3 && !markedTwice(pixel)) break;
             }
-			// If retracing path, mark it as a dead end and remove it from the solution array 
+            // If retracing path, mark it as a dead end and remove it from the solution array
             if(markedOnce(pixel)) {
                 trail = twiceTrail;
                 solution.pop();
             } else {
-				// Add the current path to the solution
-            	solution.push(direction);
-			}
+                // Add the current path to the solution
+                solution.push(direction);
+            }
             move(direction);
         }
-		// Redraw maze and replay solution path
-		//x = Start['x'];
-		//y = Start['y'];
+        // Redraw maze and replay solution path
+        //x = Start['x'];
+        //y = Start['y'];
         //ctx.drawImage(maze,0,0);
-		//solution.forEach(move);
-		return solution;
+        //solution.forEach(move);
+        return solution;
     }
 
     /**
